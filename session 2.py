3646d5167c4fe999ec0ccc2cf2afcf02f7e19c20
anikanastarin <35036242+anikanastarin@users.noreply.github.com>
2019-02-09 08:35:16 +0600

    Create session 2.py

---
+++ session 2.py
@@ -0,0 +1,60 @@
+
+'''# problem 1 : asks the user to enter their name and their age. Print out the name and age.
+name = input("Enter user's name : ")
+age = input("Enter user's age : ")
+
+print ("User's name is :  ",name)
+print ("User's age is : ",age)'''
+
+
+
+'''# problem 2 : accepts the radius of a circle from the user and compute the area.
+radius = float(input ("Enter the radius: "))
+pai = 3.1416
+Area = pai *(radius**2)
+print ("Area is :",Area)'''
+
+
+'''# problem 3: to calculate the sum of three given numbers (accept from the user).
+a = float(input("Enter the value of a : "))
+b = float(input("Enter the value of b : "))
+c = float(input("Enter the value of c : "))
+sum = a+b+c
+
+print ("Summation of the given numbers is :",sum)'''
+
+
+'''# problem 4 : accept the base and height of a triangle and compute the area. 
+base = float(input("Enter the value of base : "))
+height = float(input("Enter the value of height : "))
+
+area = (base*height)/2
+
+print("Area of the triangle is : ",area)'''
+
+
+'''# problem 5 : accepts the user's first and last name and print them in reverse order with a space between them.
+first_name = input("Enter user's first name : ")
+last_name = input("Enter user's last name : ")
+print(first_namee[::-1])
+print(last_name[::-1])'''
+
+
+
+'''# problem 6 :accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers.
+
+tuple1 = (1, 2, 3, 4, 5, 6, 7 );
+print("tuple1[1:5]: ", tuple1[1:5]);
+
+list1 = [1, 2, 3, 4, 5 ];
+print ("list1[1:5]: ", list1[1:5]);'''
+
+'''# problem 7 : display the first and last colors from the following list...color_list = ["Red","Green","White" ,"Black"]
+
+color_list = ["Red","Green","White" ,"Black"]
+print ("The first and last color of the list is :",color_list[0], color_list[3])'''
+
+
+
+
+
